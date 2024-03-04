a7b17487bd18f18947fb5ddc90b81422604c000b
SaurabhMishra779 <smishra.cs@gmail.com>
2024-03-03 22:43:14 -0800

    Requiremente and setup added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'signLanguages',
+    version= '0.0.0',
+    author= 'Saurabh',
+    author_email= 'smishra.cs@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
